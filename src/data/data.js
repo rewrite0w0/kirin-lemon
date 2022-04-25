c552beb209489fd3533b36becaf9c94b4f82eaf2
Oh TaeJun <rewrite0w0@gmail.com>
2022-04-25 13:13:16 +0900

    mod: image path

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@ export const project = Object.freeze([
   {
     title: '오늘뭐하지?',
     description: 'React Native 기반 메모장',
-    img: '/img/onulmohaji.png',
+    img: 'img/onulmohaji.png',
     codeLink: 'https://github.com/rewrite0w0/onulmohaji',
     liveLink:
       'https://play.google.com/store/apps/details?id=com.react_native_todo_list',
@@ -12,7 +12,7 @@ export const project = Object.freeze([
   {
     title: '메일 주문 프로그램',
     description: '네이버 스마트 스토어 페이지 병용 프로그램',
-    img: '/img/mailOrder.png',
+    img: 'img/mailOrder.png',
     codeLink: '',
     liveLink: 'https://mail-order.herokuapp.com/',
     report: 'mail-order',
@@ -21,7 +21,7 @@ export const project = Object.freeze([
   {
     title: '고장났단마리오',
     description: '배관 이상 검출 목적 원격 조종 탐사로봇',
-    img: '/img/mario.png',
+    img: 'img/mario.png',
     codeLink: '',
     liveLink: 'https://www.youtube.com/watch?v=6vx_JwwLTXQ',
     report: 'mario',
